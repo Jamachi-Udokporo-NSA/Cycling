cc0baf68b041e84aadac2f342651f1bd18b6f426
Tomas <cozenste@cardiff.ac.uk>
2019-03-19 13:31:43 +0000

    Implementation of Nav Bar Complete, changes to menu items required

--- Android App/app/src/main/java/com/nsa/cecobike/MainActivity.java
+++ Android App/app/src/main/java/com/nsa/cecobike/MainActivity.java
@@ -2,12 +2,17 @@ package com.nsa.cecobike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar supportActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
