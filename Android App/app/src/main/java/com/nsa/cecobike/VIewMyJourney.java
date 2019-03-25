f51be5a37f3947b7fecbc358293cf0e136c6d083
Tomas <cozenste@cardiff.ac.uk>
2019-03-25 16:24:40 +0000

    Began Implementation of Database

--- Android App/app/src/main/java/com/nsa/cecobike/VIewMyJourney.java
+++ Android App/app/src/main/java/com/nsa/cecobike/VIewMyJourney.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-//This will display the view my journey page fragment
 public class VIewMyJourney extends Fragment {
     public VIewMyJourney() {
         // Required empty public constructor
