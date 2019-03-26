9d30a2aae6c27462518b46034e861dd0362a0f42
Tomas <cozenste@cardiff.ac.uk>
2019-03-26 00:03:35 +0000

    Further Implementation Required Need to fix issue

--- Android App/app/src/main/java/com/nsa/cecobike/VIewMyJourney.java
+++ Android App/app/src/main/java/com/nsa/cecobike/VIewMyJourney.java
@@ -8,12 +8,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VIewMyJourney extends Fragment implements AdapterView.OnItemClickListener {
+    //    View Creation
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        // Init
+        ArrayAdapter();
+        View v = inflater.inflate(R.layout.fragment_view_my_journey, container, false);
+        return v;
+    }
+
     public VIewMyJourney() {
         // Required empty public constructor
 
@@ -21,10 +35,13 @@ public class VIewMyJourney extends Fragment implements AdapterView.OnItemClickLi
 
 //    Database Implementation
     Activity context;
+
+
     //List Adapter
     private void ArrayAdapter(){
         //Obtain the data
-        final List<Journey> listOfJourneys = new ArrayList<>();
+        ArrayList<String> listOfJourneys = new ArrayList<>();
+        listOfJourneys.add(getString(R.string.contents_of_list_row));
 
         final JourneyDatabase db = Room.databaseBuilder(
                 getActivity().getApplicationContext(),
@@ -38,18 +55,24 @@ public class VIewMyJourney extends Fragment implements AdapterView.OnItemClickLi
                 List<Journey> journeys = db.journeyDao().getAllJourneys();
             }
         });
+
+        //Create the adapter and connect to the data
+//        ArrayAdapter<Journey> adapter = new ArrayAdapter<Journey>(
+//                getActivity().getApplicationContext(),
+//                android.R.layout.simple_list_item_1,
+//                Adapter.
+//        );
+
+        //Fetch the listview and connect to the adapter
+//        ListView lv_journeys = getView().findViewById(R.id.lv_journeys); //Make sure that your listview in your layout file has this id
+//        lv_journeys.setAdapter(adapter);
+
+        //Set this activity to be the event listener for when list items are pressed
+//        lv_journeys.setOnItemClickListener(this);
+
     }
 
-//    View Creation
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-
-        View v = inflater.inflate(R.layout.fragment_view_my_journey, container, false);
-        return v;
-    }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
