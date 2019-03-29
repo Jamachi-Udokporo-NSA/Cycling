f05ca9a2ff5c202b9635b0bfbc30bb950fbbf9be
Udokporo Jamachi <UdokporoJB@cardiff.ac.uk>
2019-03-29 03:22:22 +0000

    Implenting the dialog fragment

---
+++ Android App/app/src/main/java/com/nsa/cecobike/Dialogboxaction.java
@@ -0,0 +1,25 @@
+package com.nsa.cecobike;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class Dialogboxaction extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information")
+                .setMessage("Enter info here")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        return builder.create();
+
+    }
+}
