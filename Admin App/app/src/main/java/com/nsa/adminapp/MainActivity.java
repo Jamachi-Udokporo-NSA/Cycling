2c3f0dc54deb7f9f7166d9c0bcd92ca7f94fae38
Daniel Fulop <FulopD@cardiff.ac.uk>
2019-03-29 11:43:41 +0000

    Resolve runtime error

--- Admin App/app/src/main/java/com/nsa/adminapp/MainActivity.java
+++ Admin App/app/src/main/java/com/nsa/adminapp/MainActivity.java
@@ -1,11 +1,10 @@
 package com.nsa.adminapp;
 
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
