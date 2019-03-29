b20acdd47593ae15c422fc2c3e693d35f459cab5
Daniel Fulop <FulopD@cardiff.ac.uk>
2019-03-29 14:51:43 +0000

    Add simple nav view - has error

--- Admin App/app/src/main/java/com/nsa/adminapp/MainActivity.java
+++ Admin App/app/src/main/java/com/nsa/adminapp/MainActivity.java
@@ -1,17 +1,34 @@
 package com.nsa.adminapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.draw_layout);
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                drawerLayout.addDrawerListener(toggle);
+                toggle.syncState();
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.start_fragment, new LoginScreen()).commit();
+//            navigationView.setCheckedItem(R.id.nav_track_my_journey);
         }
+
     }
 
 }
