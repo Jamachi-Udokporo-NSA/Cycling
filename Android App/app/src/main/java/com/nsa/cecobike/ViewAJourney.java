74cc66e7ba0475ebb1679f2cfa7ffe909b626911
Saumik Dhar <Dhars@cardiff.ac.uk>
2019-03-30 13:59:31 +0000

    implement viewajourney fragment

---
+++ Android App/app/src/main/java/com/nsa/cecobike/ViewAJourney.java
@@ -0,0 +1,28 @@
+package com.nsa.cecobike;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ViewAJourney extends Fragment {
+
+    public ViewAJourney() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_ajourney, container, false);
+    }
+}
