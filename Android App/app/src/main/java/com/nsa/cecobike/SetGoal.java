6111ae5d2774aeeedfdbc6617b08f0d6f0b75edc
Tomas <cozenste@cardiff.ac.uk>
2019-03-30 21:27:57 +0000

    Code Review Not Working Code in SetGoal Uncomment and Review

--- Android App/app/src/main/java/com/nsa/cecobike/SetGoal.java
+++ Android App/app/src/main/java/com/nsa/cecobike/SetGoal.java
@@ -1,25 +1,25 @@
 package com.nsa.cecobike;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class SetGoal extends Fragment {
+    SeekBar seekBar;
+    TextView textView;
+
     public SetGoal() {
         // Required empty public constructor
     }
 
-    public static SetGoal newInstance(String param1, String param2) {
-        SetGoal fragment = new SetGoal();
-        Bundle args = new Bundle();
-
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+//    Seekbar set goal in Miles
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +28,33 @@ public class SetGoal extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_set_goal, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_goal, container, false);
+        return v;
+    }
+
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //        Seekbar Code Below
+//        seekBar = seekBar.findViewById(R.id.seek_bar);
+//        textView = textView.findViewById(R.id.goal_miles);
+//
+//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                textView.setText(progress+"");
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
     }
 }
