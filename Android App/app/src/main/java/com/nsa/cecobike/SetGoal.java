e4d126cd595b2d9b6acd0b39c616e2ef416ec94a
Tomas <cozenste@cardiff.ac.uk>
2019-04-01 10:48:52 +0100

    Fully Implemented Seekbar and Goal Setter

--- Android App/app/src/main/java/com/nsa/cecobike/SetGoal.java
+++ Android App/app/src/main/java/com/nsa/cecobike/SetGoal.java
@@ -31,34 +31,26 @@ public class SetGoal extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.fragment_set_goal, container, false);
-        seekBar = (SeekBar) v.findViewById(R.id.seek_bar);
-        textView = (TextView) v.findViewById(R.id.goal_miles);
-        return v;
-    }
-
-    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
+        seekBar = v.findViewById(R.id.seek_bar);
+        textView = v.findViewById(R.id.goal_miles);
 
 //                Bike Miles
-//        String i = (String) textView.getText();
-//        final Double bMiles = Double.valueOf(i);
+        String i = "0";
+        textView.setText(i);
+        final Double bMiles = Double.valueOf(textView.getText().toString());
 
 //                Car miles
-//        final Double cMiles = bMiles * (1.0 / 1.609);
-        //Goal Constructor to hold the values.
-//        final Goal goal = new Goal(bMiles, cMiles);
-
-        //        Seekbar Code Below
-        seekBar = seekBar.findViewById(R.id.seek_bar);
-        textView = textView.findViewById(R.id.goal_miles);
+        final Double cMiles = bMiles * (1.0 / 1.609);
+//        Goal Constructor to hold the values.
+        final Goal goal = new Goal(bMiles, cMiles);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textView.setText(progress+"");
+                textView.setText(progress + "");
+                Log.d(TAG, "onProgressChanged: " + progress);
             }
 
             @Override
@@ -68,10 +60,62 @@ public class SetGoal extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-//                goal.setGoal_Miles(bMiles);
-//                goal.setAverage_Miles(cMiles);
-//                Log.d(TAG, "onStopTrackingTouch: " + bMiles + cMiles);
+                goal.setGoal_Miles(bMiles);
+                goal.setAverage_Miles(cMiles);
+                Log.d(TAG, "onStopTrackingTouch: " + bMiles + cMiles);
             }
         });
+
+
+        return v;
+    }
+
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        final int initVal = 0;
+//
+//
+//        SeekBar seekBar;
+//        final TextView textView;
+//
+//        //        Seekbar Code Below
+//
+//        seekBar = view.findViewById(R.id.seek_bar);
+//        textView = view.findViewById(R.id.goal_miles);
+//
+////                Bike Miles
+//        textView.setText(initVal);
+//        String i = (String) textView.getText();
+//        final Double bMiles = Double.valueOf(i);
+//
+//
+//
+////                Car miles
+//        final Double cMiles = bMiles * (1.0 / 1.609);
+////        Goal Constructor to hold the values.
+//        final Goal goal = new Goal(bMiles, cMiles);
+//
+//
+//
+//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+////                textView.setText(progress+"");
+//                Log.d(TAG, "onProgressChanged: " + progress);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+////                goal.setGoal_Miles(bMiles);
+////                goal.setAverage_Miles(cMiles);
+////                Log.d(TAG, "onStopTrackingTouch: " + bMiles + cMiles);
+//            }
+//        });
     }
 }
