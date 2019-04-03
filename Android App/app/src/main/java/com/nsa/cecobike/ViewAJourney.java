e24c5f626be9aacd6b0134a07106eb7c9c92eac8
Saumik Dhar <Dhars@cardiff.ac.uk>
2019-04-03 19:00:27 +0100

    Implement polyline to view a journey

--- Android App/app/src/main/java/com/nsa/cecobike/ViewAJourney.java
+++ Android App/app/src/main/java/com/nsa/cecobike/ViewAJourney.java
@@ -2,10 +2,14 @@ package com.nsa.cecobike;
 
 import android.arch.persistence.room.Room;
 import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,12 +19,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.squareup.okhttp.internal.InternalCache;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +40,8 @@ public class ViewAJourney extends Fragment implements OnMapReadyCallback {
     private List<Journey> listOfJourneys;
     private JourneyDatabase db;
     private GoogleMap mMap;
+    int text;
+    ArrayList<Point> coordinates = new ArrayList<>();
 
     public ViewAJourney() {
         // Required empty public constructor
@@ -68,14 +80,18 @@ public class ViewAJourney extends Fragment implements OnMapReadyCallback {
                     @Override
                     public void run() {
                         listOfJourneys = journeys;
-                        Log.d("test journey id", String.valueOf(listOfJourneys.get(0).getJid()));
+                        Log.d("test journey id", String.valueOf(listOfJourneys.get(text).toString()));
                         Bundle bundle = getArguments();
-                        int text = bundle.getInt("Journey id");
+                        text = bundle.getInt("Journey id");
                         Log.d("actual journey id", String.valueOf(text));
                         TextView timeAndDateText = v.findViewById(R.id.time_and_date_text);
+                        TextView distAndDurationText = v.findViewById(R.id.distance_and_duration_text);
+                        TextView emissionsText = v.findViewById(R.id.emissions_text);
 //                        JourneyText.setText(String.format("Date: %s%s%sDistance: %s Miles%s%sDuration: %ss", listOfJourneys.get(text).getDate(), System.lineSeparator(), System.lineSeparator(), listOfJourneys.get(text).getDistance(), System.lineSeparator(), System.lineSeparator(), listOfJourneys.get(text).getDuration()));
                         timeAndDateText.setText("Date: " + android.text.format.DateFormat.format("dd-MM-yyyy", (listOfJourneys.get(text).getDate())) + "  Time: " + android.text.format.DateFormat.format("HH:mm:ss a" ,listOfJourneys.get(text).getDate()));
-//                                + System.lineSeparator() + System.lineSeparator() + "Distance: " + listOfJourneys.get(text).getDistance() + " Km"+ System.lineSeparator() + System.lineSeparator() + "Duration: " + listOfJourneys.get(text).getDuration());
+                        distAndDurationText.setText("Distance: " + listOfJourneys.get(text).getDistance() + " Km" + "   Duration: " + listOfJourneys.get(text).getDuration()+"s");
+                        emissionsText.setText("Emissions saved: " + (listOfJourneys.get(text).getDistance() * 271) + "g");
+                        // mMap.addMarker(new MarkerOptions().position(new LatLng(listOfJourneys.get(text).getCoordinates().get(0).getpLat() , listOfJourneys.get(text).getCoordinates().get(0).getpLon())).title("Start location"));
                     }
                 });
             }
@@ -86,5 +102,31 @@ public class ViewAJourney extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(new LatLng(listOfJourneys.get(text).getCoordinates().get(0).getpLat() , listOfJourneys.get(text).getCoordinates().get(0).getpLon())).title("Start location"));
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(listOfJourneys.get(text).getCoordinates().get(0).getpLat() , listOfJourneys.get(text).getCoordinates().get(listOfJourneys.get(text).getCoordinates().size()).getpLon())).title("End location"));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target((new LatLng(listOfJourneys.get(text).getCoordinates().get(0).getpLat() , listOfJourneys.get(text).getCoordinates().get(0).getpLon())))      // Sets the center of the map to location user
+                .zoom(10)// Sets the zoom
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        coordinates = listOfJourneys.get(text).getCoordinates();
+        for (int i = 0; i+1 < coordinates.size(); i++){
+            addPolyLinesToMap(coordinates.get(i).getpLat(), coordinates.get(i).getpLon(), coordinates.get((i+1)).getpLat(),coordinates.get((i+1)).getpLon());
+//            mMap.addMarker(new MarkerOptions().position(new LatLng(coordinates.get(i).getpLat(),coordinates.get(i).getpLon())).title("End location"));
+
+        }
+    }
+
+    public void addPolyLinesToMap(final Double pLat1, final Double pLon1, final Double pLat2, final Double pLon2) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+
+            @Override
+            public void run() {
+                    PolylineOptions polyline = new PolylineOptions().add(new LatLng(pLat1, pLon1))
+                            .add(new LatLng(pLat2, pLon2)).width(20).color(Color.BLUE).geodesic(true);
+                    mMap.addPolyline(polyline);
+            }
+        });
     }
 }
