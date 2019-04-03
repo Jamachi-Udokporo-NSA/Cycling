c9d26c7cf7f5349008388db0c5f0649a91bd407c
Tomas <cozenste@cardiff.ac.uk>
2019-04-03 13:32:32 +0100

    Resolve

--- Android App/app/src/main/java/com/nsa/cecobike/JourneySummary.java
+++ Android App/app/src/main/java/com/nsa/cecobike/JourneySummary.java
@@ -24,17 +24,7 @@ public class JourneySummary extends Fragment {
     private List<Journey> listOfJourneys;
     private JourneyDatabase db;
     Button closebutton;
-
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
