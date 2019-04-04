6eb647295b54ea6fcde4d7f85c57349a81493d73
Daniel Fulop <FulopD@cardiff.ac.uk>
2019-04-04 02:52:30 +0100

    implement json import for locations

--- Admin App/app/src/main/java/com/nsa/adminapp/HomePage.java
+++ Admin App/app/src/main/java/com/nsa/adminapp/HomePage.java
@@ -24,10 +24,6 @@ public class HomePage extends Fragment {
 
         main.onCreate(savedInstanceState);
 
-
-
-
-
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- Admin App/app/src/main/java/com/nsa/adminapp/Maps.java
+++ Admin App/app/src/main/java/com/nsa/adminapp/Maps.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,14 +14,31 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
 
 public class Maps extends Fragment implements OnMapReadyCallback {
-
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
     private GoogleMap mMap;
+    private List<LatLng> list = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addHeatMap();
+
 
     }
     @Override
@@ -45,10 +63,49 @@ public class Maps extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng cardiff = new LatLng(51.495624, -3.176227);
+//        LatLng cardiff = new LatLng(51.495624, -3.176227);
+        LatLng cardiff = new LatLng(-37.1886, 145.708);
         mMap.addMarker(new MarkerOptions().position(cardiff).title("Marker in Cardiff"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(cardiff));
+        for (LatLng latLng : list) {
+            mMap.addMarker(new MarkerOptions().position(latLng));
+        }
+
+
+
+
+    }
+    protected GoogleMap getMap() {
+
+        return mMap;
+    }
+    private void addHeatMap() {
+
+
+        try {
+            list = readItems(R.raw.police);
+        } catch (JSONException e) {
+            Toast.makeText(this.getActivity(), "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                .build();
+    }
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
     }
 }
